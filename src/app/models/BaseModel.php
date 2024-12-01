388e6f2ad1022444a2b0bfddb6398e47a4741922
MC-THEI <th@mc-kg.de>
2024-12-01 19:53:32 +0100

    Enhance BaseModel with dynamic query generation and column retrieval

--- src/app/models/BaseModel.php
+++ src/app/models/BaseModel.php
@@ -1,6 +1,11 @@
 <?php
 
 namespace app\models;
+
+use app\traits\PrepareConditions;
+use app\traits\PrepareFields;
+use app\traits\PrepareOrder;
+use app\traits\PreparePagination;
 use PDO;
 
 abstract class BaseModel
@@ -13,19 +18,49 @@ abstract class BaseModel
         $this->conn = $db;
     }
 
+    private function getTableColumns(): array
+    {
+        $query = "DESCRIBE " . $this->table;
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        $columns = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+        return $columns ?: [];
+    }
+
     public function getAll()
     {
-        $query = "SELECT * FROM " . $this->table;
+        $fields = $this->prepareFields();
+        [$conditions, $bindParams] = $this->prepareConditions();
+        $order = $this->prepareOrder();
+        $pagination = $this->preparePagination();
+
+        $query = "SELECT $fields FROM " . $this->table . $conditions . $order . $pagination;
+
         $stmt = $this->conn->prepare($query);
+        foreach ($bindParams as $param => $value) {
+            $stmt->bindValue($param, $value);
+        }
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+
+
     public function getById($id)
     {
-        $query = "SELECT * FROM " . $this->table . " WHERE id = :id";
+        $fields = $this->prepareFields();
+        [$conditions, $bindParams] = $this->prepareConditions();
+
+        $query = "SELECT $fields FROM " . $this->table . " WHERE id = :id" . $conditions;
+
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(':id', $id);
+
+        foreach ($bindParams as $param => $value) {
+            $stmt->bindValue($param, $value);
+        }
+
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC) ?: null;
     }
