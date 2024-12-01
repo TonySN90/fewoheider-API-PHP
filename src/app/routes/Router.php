dcac3e1067919a8cc38f369acf6a1d3a88cc8657
MC-THEI <th@mc-kg.de>
2024-12-01 19:51:59 +0100

    Refactor Router to improve route handling and add API routes helper

--- src/app/routes/Router.php
+++ src/app/routes/Router.php
@@ -8,15 +8,17 @@ class Router
 {
     private array $routes = [];
 
-    public function add($method, $path, $controllerAction): void
+    public function addRoute(string $method, string $path, string $controllerAction): void
     {
         $this->routes[$method][$path] = $controllerAction;
     }
 
-    public function dispatch() : void
+    public function dispatch(): void
     {
         $method = $_SERVER['REQUEST_METHOD'];
         $path = $_SERVER['REQUEST_URI'];
+
+        $path = strtok($path, '?');
 
         // Singleton-DB-Connection
         $db = Database::getInstance();
@@ -30,7 +32,8 @@ class Router
 
                 if (class_exists($class) && method_exists($class, $action)) {
                     $controller = new $class($db);
-                    call_user_func_array([$controller, $action], array_slice($matches, 1));
+                    call_user_func_array([$controller, $action], array_values($matches));
+                    return;
                 }
             }
         }
@@ -39,9 +42,18 @@ class Router
         echo "404 Page not found";
     }
 
-    private function formatRoute($route): string
+    private function formatRoute(string $route): string
     {
         $route = preg_replace('/\{([^\/]+)\}/', '(?P<$1>[^/]+)', $route);
-        return "#^$route$#";
+        return "#^$route/?$#";
+    }
+
+    public function addApiRoutes(string $prefix, string $controller): void
+    {
+        $this->addRoute('GET', "$prefix", "$controller@viewAll");
+        $this->addRoute('GET', "$prefix/{id}", "$controller@viewById");
+        $this->addRoute('POST', "$prefix", "$controller@create");
+        $this->addRoute('PUT', "$prefix/{id}", "$controller@update");
+        $this->addRoute('DELETE', "$prefix/{id}", "$controller@delete");
     }
 }
